b7a3fa97573cbb27b800c44bd4db3eb2207be6e0
akanz <samuelakande2014@yahoo.com>
2022-04-18 12:59:24 +0100

    meta preview

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,13 @@ export default function Document() {
             <Head>
                 <meta name="theme-color" content="#fff" />
                 <meta name="description" content="AswangTribe brings out the creative side of it's community members." />
-                <meta property="og:title" content="" />
+                <meta property="og:title" content="AswangTribe brings out the creative side of it's community members." />
                 <meta property="og:description" content="AswangTribe brings out the creative side of it's community members." />
                 <meta property="og:image" content="https://i.ibb.co/xMm0cf7/aswang.jpg" />
 
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://aswangfansite.vercel.app/" />
-                <meta name="twitter:title" content="" />
+                <meta name="twitter:title" content="AswangTribe brings out the creative side of it's community members." />
                 <meta name="twitter:description" content="AswangTribe brings out the creative side of it's community members." />
                 <meta name="twitter:image" content="https://i.ibb.co/xMm0cf7/aswang.jpg" />
                 <link rel="icon" href="/aswang.jpg" />
